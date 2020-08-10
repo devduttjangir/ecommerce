4e37cc623d43b6d40b5336df42c207e9b3cfb214
devjangir <devpassion7@gmail.com>
2020-08-10 09:52:21 +0530

    project setup and folder structure created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Root from './Components/Root'
+import About from './Components/About'
+
+function App() {
+  return (
+    <div>
+      <Root />
+      <About />
+    </div>
+  );
+}
+
+export default App;
