c0111b87de0d02e9f449affd801cc08899422169
devjangir <devpassion7@gmail.com>
2020-08-12 09:17:34 +0530

    Routing Implemented and Not found page handled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,48 @@
-import React from 'react';
-const Header=()=>{
-    return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Miro</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Posts</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" >Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    )
-}
+import React from "react";
+import Routes from "../../Utility/Routes";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Miro
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              <Link to={Routes.home.path}>{Routes.home.title}</Link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <Link to={Routes.about.path}>{Routes.about.title}</Link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <Link to={Routes.posts.path}>{Routes.posts.title}</Link>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <Link to={Routes.contact.path}>{Routes.contact.title}</Link>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 export default Header;
