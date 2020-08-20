df7ca29ca6259fc7c2cfaa2acf4c8bf4f7285c78
devdutt3Pillar <devdutt.jangir@modmed.com>
2020-08-20 10:59:21 +0530

    chat code changes

--- src/Components/MessageContainer/Chatts.js
+++ src/Components/MessageContainer/Chatts.js
@@ -1,39 +1,36 @@
-import React from "react"; 
+import React from "react";
 
-const Chatts=(props)=>
-{
-    const renderchatts=()=>{
-        return props.chatDetails.map((chatDetail)=>{
-            return (
-
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <div className="jumbotron jumbotron-fluid">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col">
-                                <img src={chatDetail.imageUrl} height="50%"/>
-                                </div>
-                                <div className="col">
-                                {chatDetail.userName}
-                                </div>
-                                <div className="col">
-                                {chatDetail.postdate}
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col">
-                                {chatDetail.content}
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                
+const Chats = (props) => {
+  const chatComponent = (chatDetail) => {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <div className="row">
+                  <div className="col">
+                    <img src={chatDetail.imageUrl} height="50%" />
+                  </div>
+                  <div className="col">{chatDetail.userName}</div>
+                  <div className="col">{chatDetail.postdate}</div>
                 </div>
+                <div className="row">
+                  <div className="col">{chatDetail.content}</div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
-export default Chatts;
+      </div>
+    );
+  };
+
+  const renderChats = () => {
+    return props.chatDetails.map((chatDetail) => {
+      return chatComponent(chatDetail);
+    });
+  };
+  return <div>{renderChats()}</div>;
+};
+export default Chats;
